48f7c0ce54f28edfe2c7c478bf3ea13f503c84b1
aliyaghobi <aliymix07@gmail.com>
2022-03-22 13:26:52 +0430

    getCurrentWeather in current_weather.dart

--- lib/current_weather.dart
+++ lib/current_weather.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:p_12_api_weather/models/weather.dart';
-
+import 'package:http/http.dart' as http;
 class CurrentWeatherPage extends StatefulWidget {
   const CurrentWeatherPage({Key? key}) : super(key: key);
 
@@ -27,4 +29,19 @@ class _CurrentWeatherPageState extends State<CurrentWeatherPage> {
       ],
     );
   }
+  Future getCurrentWeather()async{
+    Weather weather;
+    String city="shiraz";
+    String apiKey="YOUR API KEY";
+    var url ="http://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric";
+    final response = await http.get(Uri.parse(url));
+
+    if(response.statusCode == 200){
+      weather= Weather.fromJson(jsonDecode(response.body));
+    }
+    else{
+      // TODO:Throw error here
+    }
+  }
 }
+//after appid= into varible utl-> bad4ad67fe76157b5bc1ef846793019d
