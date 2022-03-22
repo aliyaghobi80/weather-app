2869c2cc3f802c3be7941e90b98fa26dac2f8cf1
aliyaghobi <aliymix07@gmail.com>
2022-03-22 12:17:45 +0430

    added widget weatherBox in current_weather.dart

--- lib/current_weather.dart
+++ lib/current_weather.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:p_12_api_weather/models/weather.dart';
 
 class CurrentWeatherPage extends StatefulWidget {
   const CurrentWeatherPage({Key? key}) : super(key: key);
@@ -10,8 +11,20 @@ class CurrentWeatherPage extends StatefulWidget {
 class _CurrentWeatherPageState extends State<CurrentWeatherPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(
-      child: Text('My Weather App'),
-    ),);
+    return Scaffold(
+      body: Center(
+        child: Text('My Weather App'),
+      ),
+    );
+  }
+  Widget weatherBox(Weather _weather){
+    return Column(
+      children: [
+        Text('${_weather.temp}ºC'),
+        Text('${_weather.description}'),
+        Text('Feels:${_weather.feelsLike}ºC'),
+        Text('H:${_weather.high}ºC L:${_weather.low}ºC'),
+      ],
+    );
   }
 }
