477a7a71c915ee5df2bde31088bdccb8149fb8b5
aliyaghobi <aliymix07@gmail.com>
2022-03-24 12:49:52 +0430

    add text style normal and temp

---
+++ lib/constant.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kTextStyleNormal=TextStyle(fontSize: 20,fontWeight: FontWeight.w400);
+const kTextStyleTemp=TextStyle(fontSize: 55,fontWeight: FontWeight.w400);
