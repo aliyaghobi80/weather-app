ab518525a1032e10d6df0e953258d97f93d081b8
aliyaghobi <aliymix07@gmail.com>
2022-03-25 21:54:24 +0430

    Format Code

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -1,22 +1,35 @@
-
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
-
   TextEditingController city;
   VoidCallback onPressed;
-  CustomTextField({Key? key, required this.city,required this.onPressed}) : super(key: key);
+
+  CustomTextField({Key? key, required this.city, required this.onPressed})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return TextField(
+return    TextField(
       controller: city,
       decoration: InputDecoration(
         border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(20),
+          borderRadius: BorderRadius.circular(10),
         ),
         label: Text('City'),
         labelStyle: TextStyle(fontSize: 20),
-        icon:IconButton(onPressed: onPressed, icon: Icon(Icons.search)),
+        icon: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.all(15),
+              primary: Colors.tealAccent.withOpacity(0.1),
+              shadowColor: Colors.yellow,
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              side: BorderSide(
+                  color: Colors.yellow.withOpacity(0.6), width: 1.7)),
+          onPressed: onPressed,
+          child: Icon(Icons.search, color: Colors.black),
+        ),
       ),
     );
   }
